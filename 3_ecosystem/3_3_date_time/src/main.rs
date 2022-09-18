8e528f3744fafbdf174b39a147107097afd64371
Богдан Балушкин <bogdan.balushkin5@gmail.com>
2022-09-18 20:54:31 +0300

    Step 3.3 ready for review

--- 3_ecosystem/3_3_date_time/src/main.rs
+++ 3_ecosystem/3_3_date_time/src/main.rs
@@ -1,3 +1,6 @@
+use chrono::prelude::*;
+
+
 fn main()
 {
   println!( "Implement me!" );
@@ -5,17 +8,36 @@ fn main()
 
 const NOW : &str = "2019-06-26";
 
-struct User;
+struct User
+{
+  birthdate : Date< Utc >,
+}
 
 impl User
 {
-  fn with_birthdate( year : i32, month : u32, day : u32 ) -> Self { unimplemented!() }
+  fn with_birthdate( year : i32, month : u32, day : u32 ) -> Self
+  {
+    User{ birthdate : Utc.ymd( year, month, day ) }
+  }
 
   /// Returns current age of [ `User`] in years.
-  fn age( &self ) -> u16 { unimplemented!() }
+  fn age( &self ) -> u16
+  {
+    let native_date = NaiveDate::parse_from_str( NOW, "%Y-%m-%d" ).unwrap();
+    let date_time = Utc.from_local_date( &native_date ).unwrap();
+    
+    match date_time.years_since( self.birthdate )
+    {
+      None => 0,
+      Some( years ) => years as u16
+    }
+  }
 
   /// Checks if [ `User`] is 18 years old at the moment.
-  fn is_adult( &self ) -> bool { unimplemented!() }
+  fn is_adult( &self ) -> bool
+  {
+    self.age() >= 18
+  }
 }
 
 #[ cfg( test ) ]
@@ -33,6 +55,7 @@ mod age_spec
       ( ( 0, 1, 1 ), 2019 ),
       ( ( 1970, 1, 1 ), 49 ),
       ( ( 2019, 6, 25 ), 0 ),
+      ( ( 2019, 6, 25 ), 0 ),
     ]
     {
       let user = User::with_birthdate( y, m, d );
@@ -46,13 +69,30 @@ mod age_spec
     for ( ( y, m, d ), expected ) in vec!
     [
       ( ( 2032, 6, 25 ), 0 ),
-      ( ( 2016, 6, 27 ), 0 ),
+      // ( ( 2016, 6, 27 ), 0 ), // ? is it correct test?
+      ( ( 2019, 6, 27 ), 0 ),
       ( ( 3000, 6, 27 ), 0 ),
       ( ( 9999, 6, 27 ), 0 )
     ]
     {
       let user = User::with_birthdate( y, m, d );
       assert_eq!( user.age(), expected );
+    }
+  }
+
+  #[ test ]
+  fn adult()
+  {
+    for ( ( y, m, d ), expected ) in vec!
+    [
+      ( ( 1990, 6, 4 ), true ),
+      ( ( 2001, 6, 24 ), true ),
+      ( ( 2001, 6, 27 ), false ),
+      ( ( 2019, 6, 25 ), false ),
+    ]
+    {
+      let user = User::with_birthdate( y, m, d );
+      assert_eq!( user.is_adult(), expected );
     }
   }
 }
