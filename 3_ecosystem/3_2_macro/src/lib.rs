af3cf0ac205060277c3b476d5610a25dc72d2e69
Богдан Балушкин <bogdan.balushkin5@gmail.com>
2022-09-18 19:05:55 +0300

    Step 3.2 ready for review

---
+++ 3_ecosystem/3_2_macro/src/lib.rs
@@ -0,0 +1,36 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{ parse_macro_input, Expr, parse::Parse, punctuated::Punctuated, Token };
+
+struct Args
+{
+  vars : Vec< Expr >
+}
+
+impl Parse for Args
+{
+    fn parse( input : syn::parse::ParseStream ) -> syn::Result< Self >
+    {
+      let vars = Punctuated::< Expr, Token![ , ] >::parse_terminated( input )?;
+      Ok
+      (
+        Args { vars : vars.into_iter().collect() }
+      )
+    }
+}
+
+#[ proc_macro ]
+pub fn pbtreemap( input : TokenStream ) -> TokenStream
+{
+  let input = parse_macro_input!( input as Args ).vars;
+  TokenStream::from( quote!
+  {{
+      let mut tmp_btreemap = BTreeMap::new();
+
+      for ( key, value ) in [ #(#input),* ]
+      {
+        tmp_btreemap.insert( key, value );
+      }
+      tmp_btreemap
+  }})
+}
